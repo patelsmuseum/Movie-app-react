6b5943e8c8100274e585b35a7924926b604b405b
Purshotam <pankajpurshotam@gmail.com>
2023-11-16 14:36:56 +0530

    redux store added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+
+import {createStore} from 'redux'
+const store = createStore(movies);
+
+console.log(store.getState());
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
